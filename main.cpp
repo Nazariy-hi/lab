417fce276e14e7da626fcd92ca4aec5dedaa1586
Nazariy-hi <77990661+Nazariy-hi@users.noreply.github.com>
2021-01-26 21:39:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+ #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-	float a,b,c;
-	cin>>a>>b;
-	c=a+b;
+float a,b,s,p;
+cin>>a>>b;
+s=a*b;
+p=2*(a+b);
+cout<<"a * b = " << s << '\n';
+cout<<"2*(a+b) = "<<p;
 
-	cout<<"a+b ="<<c;
-	return 0;
+return 0;
 }
