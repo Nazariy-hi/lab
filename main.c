0b01ef71a22a7842d7eaf628eb86b4fc1c15dff4
Nazariy-hi <77990661+Nazariy-hi@users.noreply.github.com>
2021-01-26 17:57:41 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	// Declare an integer variable
+	int n;
+	// Change the value of the variable
+	n = 5;
+	// Print Hello World! on the screen
+	printf("Hello World! \n");
+	// More printings
+	printf("This is my first program. \n");
+	printf("I am learning C programming. \n");
+	
+	//Change the value of the variants
+	n = n + 1;
+	
+	// The program pauses too se the console output
+	system ("PAUSE");
+	return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+float a,b,s,p;
+cin>>a>>b;
+s=a*b;
+p=2*(a+b);
+cout<<"a * b = " << s << '\n';
+cout<<"2*(a+b) = "<<p;
+
+return 0;
+}
