6bcf49fa3384787bf8e068bf0c6934d894f5198c
Nazariy-hi <77990661+Nazariy-hi@users.noreply.github.com>
2021-01-26 17:58:26 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
 
-int main()
+int main() 
 {
-float a,b,s,p;
-cin>>a>>b;
-s=a*b;
-p=2*(a+b);
-cout<<"a * b = " << s << '\n';
-cout<<"2*(a+b) = "<<p;
+	float a,b,c;
+	cin>>a>>b;
+	c=a+b;
 
-return 0;
+	cout<<"a+b ="<<c;
+	return 0;
 }
